14dcbec2bfb58ccb6bd238b6423d66e282b4d18c
gabrielNardone <gabriel.f.nardone@gmail.com>
2023-10-19 17:24:13 -0300

    configurado shadcn plenamente funcional

--- proyecto-integrador/src/App.jsx
+++ proyecto-integrador/src/App.jsx
@@ -1,8 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { Terminal } from "lucide-react"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-cyan-500">
-      Hello world!
-      Voten a Massa
-    </h1>
+    <div>
+      <h1 className="text-3xl font-bold underline text-cyan-500">
+        Hello world!
+        Voten a Massa
+      </h1>
+
+      <Button className="mt-2">Delete</Button>
+      
+      <Alert>
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+      </Alert>
+
+    </div>
   )
 }

37cddad443b7a0dceb6c3fe28365345089b29417
gabrielNardone <gabriel.f.nardone@gmail.com>
2023-10-19 22:59:34 -0300

    panel de administración

--- proyecto-integrador/src/App.jsx
+++ proyecto-integrador/src/App.jsx
@@ -4,17 +4,16 @@ import { ThemeProvider } from "@/components/theme/theme-provider";
 import { ThemeToggle } from "./components/theme/theme-toogle";
 import { AppRouter } from "./router/AppRouter";
 
-// Shadcn
-import { Button } from "@/components/ui/button";
 
 export default function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      {/* 
       <div className="flex items-center flex-col gap-5">
         <ThemeToggle />
+      </div> 
+      */}
         <AppRouter/>
-        <Button className="mt-2">Prueba</Button>
-      </div>
     </ThemeProvider>
   );
 }
--- proyecto-integrador/src/pages/Administrador.jsx
+++ proyecto-integrador/src/pages/Administrador.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+
+
 
 export const Administrador = () => {
   return (
-    <div>Administrador</div>
+    <div className='flex justify-center items-center h-screen'>
+      <div className='flex flex-col border-2 border-slate-100 rounded-lg	p-7'>
+
+        <h2 className='text-2xl'>Panel de administración</h2>
+
+        <Input
+          className="my-5"
+          type="text"
+          placeholder="Título"
+        />
+
+        <Textarea
+          className="mb-5"
+          placeholder="Descripción del producto"
+        />
+
+        <Button className="text-lg bg-indigo-600 text-white">Agregar producto</Button>
+
+
+      </div>
+    </div>
   )
 }

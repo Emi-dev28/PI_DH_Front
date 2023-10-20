70bdd8e70e66d5a1a487fd00b52edaa38e35f52a
gabrielNardone <gabriel.f.nardone@gmail.com>
2023-10-20 08:10:13 -0300

    small change

--- proyecto-integrador/src/main.jsx
+++ proyecto-integrador/src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { DataContextProvider } from './context/DataContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <DataContextProvider>
+      <App />
+    </DataContextProvider>
   </BrowserRouter>,
 )
 

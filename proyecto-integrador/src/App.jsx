d11c706d2da310a47278219f1e50deade05b1c5a
gustavofmacia <gustavofmacia@gmail.com>
2023-10-19 21:40:28 -0300

    Creados componentes necesarios para que funcione el modo claro y oscuro según shadcn para vite y funcionando

--- proyecto-integrador/src/App.jsx
+++ proyecto-integrador/src/App.jsx
@@ -1,25 +1,20 @@
+// Shadcn dark mode provider
+import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeToggle } from "./components/theme-toogle";
+// Shadcn
 import { Button } from "@/components/ui/button";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
-import { Terminal } from "lucide-react"
 
 export default function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-cyan-500">
-        Hello world!
-        Voten a Massa
-      </h1>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="flex items-center flex-col gap-5">
+        <ThemeToggle />
+        <h1 className="text-3xl font-bold underline text-cyan-500">
+          Hello world!
+        </h1>
 
-      <Button className="mt-2">Delete</Button>
-      
-      <Alert>
-        <Terminal className="h-4 w-4" />
-        <AlertTitle>Heads up!</AlertTitle>
-        <AlertDescription>
-          You can add components and dependencies to your app using the cli.
-        </AlertDescription>
-      </Alert>
-
-    </div>
-  )
+        <Button className="mt-2">Prueba</Button>
+      </div>
+    </ThemeProvider>
+  );
 }

e2ec778c9ca2578eb5f17db4cc13a20776536400
gustavofmacia <gustavofmacia@gmail.com>
2023-10-22 15:44:43 -0300

    corregido error menor en el export del footer

--- proyecto-integrador/src/App.jsx
+++ proyecto-integrador/src/App.jsx
@@ -7,7 +7,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { useLocation } from "react-router-dom";
 import { AppRouter } from "./router/AppRouter";
 // Components:
-import { Footer } from "@/components/Footer";
+import Footer from "@/components/Footer";
 
 export default function App() {
   const location = useLocation();

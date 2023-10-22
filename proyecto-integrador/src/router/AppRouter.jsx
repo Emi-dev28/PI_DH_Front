f501e037c7a18c758cf0984b2dd8e4d6ca71f41d
gustavofmacia <gustavofmacia@gmail.com>
2023-10-22 11:55:44 -0300

    linter

--- proyecto-integrador/src/router/AppRouter.jsx
+++ proyecto-integrador/src/router/AppRouter.jsx
@@ -1,19 +1,18 @@
-import { Administrador } from "@/pages/Administrador"
-import { Home } from "../pages/Home"
-import { Route, Routes } from "react-router-dom"
-import { Detalle } from "@/pages/Detalle"
-import { ListaDeProductos } from "@/pages/ListaDeProductos"
+import { Administrador } from "@/pages/Administrador";
+import { Home } from "../pages/Home";
+import { Route, Routes } from "react-router-dom";
+import { Detalle } from "@/pages/Detalle";
+import { ListaDeProductos } from "@/pages/ListaDeProductos";
 //import { ThemeToggle } from "@/components/theme/theme-toogle"
 
 export const AppRouter = () => {
-
-    //const {theme} = useContext(ThemeToggle)
-    return (
-            <Routes>
-                <Route path="/*" element={<Home />}></Route>
-                <Route path="/detalle" element={<Detalle />}></Route>
-                <Route path="/administrador" element={<Administrador />}></Route>
-                <Route path="/lista-productos" element={<ListaDeProductos />}></Route>
-            </Routes>
-    )
-}
+  //const {theme} = useContext(ThemeToggle)
+  return (
+    <Routes>
+      <Route path="/*" element={<Home />}></Route>
+      <Route path="/detalle" element={<Detalle />}></Route>
+      <Route path="/administrador" element={<Administrador />}></Route>
+      <Route path="/lista-productos" element={<ListaDeProductos />}></Route>
+    </Routes>
+  );
+};

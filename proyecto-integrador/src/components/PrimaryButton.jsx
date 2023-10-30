8e92bad8cfddb419fa6c05973d4c3f39bafdedab
gustavofmacia <gustavofmacia@gmail.com>
2023-10-29 21:41:59 -0300

    Se actualizó el tamaño de la letra de los PrimaryButton a text-base

--- proyecto-integrador/src/components/PrimaryButton.jsx
+++ proyecto-integrador/src/components/PrimaryButton.jsx
@@ -9,7 +9,7 @@ export default function PrimaryButton({ children, ...props }) {
   return (
     <Button
       {...props}
-      className="bg-gradient-to-b from-btnPink to-btnPinkDarker shadow-xl hover:opacity-90"
+      className="bg-gradient-to-b from-btnPink to-btnPinkDarker shadow-xl text-base hover:opacity-90"
     >
       {children}
     </Button>

fbbb171a6f20f778ceac365535cb82867afeaadb
Jose Martinez <jdm.bass89@gmail.com>
2023-11-09 22:11:59 -0300

    retoques al slider

--- proyecto-integrador/src/components/home/CardSlider.jsx
+++ proyecto-integrador/src/components/home/CardSlider.jsx
@@ -1,30 +1,30 @@
-import PrimaryButton from "../custom-ui/PrimaryButton";
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
-
 export default function CardSlider({ product }) {
-  return (
-    <Card className="w-full sm:w-3/4 md:w-1/2 lg:w-1/2 xl:w-1/3 2xl:w-1/4 h-[460px] relative flex flex-col p-4 md:p-6">
-      <div className="flex justify-center items-center mb-2">
-        <img
-          src={
-            product.images[0]
-              ? product.images[0].image
-              : "http://dummyimage.com/283x430.png/5fa2dd/ffffff"
-          }
-          className="w-full h-[240px] object-cover"
-          alt={product.name}
-        />
-      </div>
-
-      <CardHeader className="flex justify-between">
-        <div>
-          <CardTitle className="text-md md:text-lg lg:text-lg xl:text-lg 2xl:text-lg mt-2 md:mt-4 lg:mt-4 xl:mt-4 2xl:mt-4">
-            {product.name}
-          </CardTitle>
+    return (
+      <div className="w-full sm:w-3/4 md:w-1/2 lg:w-1/2 xl:w-1/3 2xl:w-1/4 h-[380px] relative flex flex-col p-4 md:p-6 rounded-md border border-gray-300 hover:border-gray-500 shadow-md transition-all duration-300 ease-in-out">
+        <div className="flex justify-center items-center mb-2">
+          <img
+            src={
+              product.images[0]
+                ? product.images[0].image
+                : "http://dummyimage.com/283x430.png/5fa2dd/ffffff"
+            }
+            className="w-full h-[180px] object-cover rounded-t-md"
+            alt={product.name}
+          />
         </div>
-      </CardHeader>
-
-      <PrimaryButton className="mt-2 md:mt-4 lg:mt-4 xl:mt-4 2xl:mt-4">Categoria</PrimaryButton>
-    </Card>
-  );
-}
+  
+        <section className="flex justify-between">
+          <div>
+            <h2 className="text-base sm:text-lg md:text-lg lg:text-lg xl:text-lg 2xl:text-lg font-semibold">
+              Nombre de categoría
+            </h2>
+          </div>
+        </section>
+  
+        <button className="mt-2 md:mt-4 lg:mt-8 xl:mt-4 2xl:mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-700 cursor-pointer transition-all duration-300 ease-in-out text-base sm:text-lg md:text-lg xl:text-base 2xl:text-lg w-full md:w-auto">
+          Ver detalles
+        </button>
+      </div>
+    );
+  }
+  
--- proyecto-integrador/src/components/home/Slider.jsx
+++ proyecto-integrador/src/components/home/Slider.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import CardSlider from '@/components/home/CardSlider';
 import PropTypes from 'prop-types';
-import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { MdArrowCircleLeft, MdArrowCircleRight } from 'react-icons/md';
 import { Button } from '@/components/ui/button';
 
 Slider.propTypes = {
@@ -10,7 +10,7 @@ Slider.propTypes = {
 
 export default function Slider({ products }) {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const itemsPerPage = 4; // Ajusta la cantidad de tarjetas por página según tus necesidades
+  const itemsPerPage = 4; 
 
   const visibleProducts = products.slice(currentIndex, currentIndex + itemsPerPage);
 
@@ -32,14 +32,17 @@ export default function Slider({ products }) {
         {visibleProducts.map((product) => (
           <CardSlider key={product.id} product={product} className="w-1/2 sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/4" />
         ))}
-
+      {/*  
         <Button variant="icon" size="icon" onClick={prevHandler} className="absolute left-0 top-1/2 transform -translate-y-1/2">
-          <MdChevronLeft className="text-3xl" />
+          <MdArrowCircleLeft className="text-4xl" />
         </Button>
 
         <Button variant="icon" size="icon" onClick={nextHandler} className="absolute right-0 top-1/2 transform -translate-y-1/2">
-          <MdChevronRight className="text-3xl" />
+          <MdArrowCircleRight className="text-4xl" />
         </Button>
+
+      */}
+      
       </div>
     </div>
   );

0ba771d0ad034d242eb3905409726a9b86eadea4
gustavofmacia <gustavofmacia@gmail.com>
2023-11-10 16:19:13 -0300

    Aplicado prettier en Navbar

--- proyecto-integrador/src/components/layout/Navbar.jsx
+++ proyecto-integrador/src/components/layout/Navbar.jsx
@@ -31,37 +31,34 @@ export const Navbar = () => {
         </Link>
       </div>
 
-   
-{/* Input de búsqueda */}
-<div className="hidden md:flex items-center">
-  <div className="relative">
-    <input
-      type="text"
-      placeholder="Buscar..."
-      value={search}
-      onChange={(e) => setSearch(e.target.value)}
-      className="p-2 pl-8 pr-4 border rounded-full focus:outline-none focus:border-blue-500 transition-all duration-300 ease-in-out hover:border-gray-400 bg-gray-100"
-      style={{ width: "450px" }} // Puedes ajustar el ancho según tus necesidades
-    />
-    <div className="absolute inset-y-0 left-0 flex items-center pl-2">
-      <svg
-        className="w-4 h-4 text-gray-600"
-        fill="none"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path d="M21 21l-6-6M9 2a7 7 0 110 14 7 7 0 010-14z"></path>
-      </svg>
-    </div>
-  </div>
-</div>
+      {/* Input de búsqueda */}
+      <div className="hidden md:flex items-center">
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="Buscar..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="p-2 pl-8 pr-4 border rounded-full focus:outline-none focus:border-blue-500 transition-all duration-300 ease-in-out hover:border-gray-400 bg-gray-100"
+            style={{ width: "450px" }} // Puedes ajustar el ancho según tus necesidades
+          />
+          <div className="absolute inset-y-0 left-0 flex items-center pl-2">
+            <svg
+              className="w-4 h-4 text-gray-600"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path d="M21 21l-6-6M9 2a7 7 0 110 14 7 7 0 010-14z"></path>
+            </svg>
+          </div>
+        </div>
+      </div>
 
-
-
-      {(role === "USER" || role === "ADMIN") ? (
+      {role === "USER" || role === "ADMIN" ? (
         <UserSessionMenu name={name} firstLetter={firstLetter} />
       ) : (
         <div className="hidden md:flex items-center gap-5">
